831e1a3608a1258d8ca6d3b6bbb528edcaa60f3f
Thomas Gomree <gomreethomas@gmail.com>
2023-11-09 15:40:17 +0100

    update file

--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 try
 {
 	// On se connecte à MySQL
-	$bdd = new PDO('mysql:host=localhost;dbname=becode;charset=utf8', 'toms', 'pwd');
+	$bdd = new PDO('mysql:host=localhost;dbname=weatherapp;charset=utf8', 'toms', 'pwd');
 }
 catch(Exception $e)
 {
 	// En cas d'erreur, on affiche un message et on arrête tout
         die('Erreur : '.$e->getMessage());
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
